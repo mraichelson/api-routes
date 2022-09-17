5d42097bbdc14cf6be061ae45cd2b0538cc7e725
Michael Raichelson <i.am@michaelraichelson.com>
2022-09-17 00:33:16 -0500

    Start to add a comment about what's up with ip.ts

--- server/api/ip.ts
+++ server/api/ip.ts
@@ -1,3 +1,7 @@
+/**
+ * Pop this into `server/api/ip.{ts/js}` and you will be able to
+ * fetch from `/api/ip` to get a visitor's IP address.
+ */
 export default defineEventHandler(async (event) => {
   return {
     ip: getRequestHeader(event, "x-nf-client-connection-ip")
